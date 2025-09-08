4902c4bcb6bd34701920485fadbd603dce9de8c3
mohamedamine-code <mohamedaminebensir09@gmail.com>
2025-09-08 12:37:00 +0100

    push

--- server.js
+++ server.js
@@ -16,10 +16,8 @@ const componentRoutes = require('./routes/Component'); // ✅ correct import
 app.use('/components', componentRoutes);
 
 // MongoDB connection
-mongoose.connect("mongodb://10.0.100.242:27017/newdata01", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect("mongodb://10.0.100.242:27017/newdata01", 
+)
 .then(() => {
   console.log('✅ Connected to MongoDBkkkkkmmmm');
 
