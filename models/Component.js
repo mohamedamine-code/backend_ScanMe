f80a86fecf02fef49e3fc9d84a5e22097e47f891
mohamedamine-code <mohamedaminebensir09@gmail.com>
2025-09-23 14:47:53 +0100

    push

--- models/Component.js
+++ models/Component.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
 const ComponentSchema = new mongoose.Schema({
-  _id: String,
-  name: String,
-  type: String,
-  quantity: Number,
-  location: String,
-  datasheet_url: String
-}, { collection: 'Component' });  // force exact collection name
+  _id:String,
+  Society_Name: String,
+  Number_of_Floors: String,
+  Technical_Room_Number: String,
+  Cabinet_Number: String,
+  Switcher: String,
+  Port: String,
+  State_Port: Boolean,
+});  // force exact collection name
 
 module.exports = mongoose.model('Component', ComponentSchema);
--- server.js
+++ server.js
@@ -8,19 +8,24 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: '*', // Allow all origins — safe for dev
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: false // unless you need cookies/auth headers
+}));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Routes
 const componentRoutes = require('./routes/Component'); // ✅ correct import
 app.use('/components', componentRoutes);
 
 // MongoDB connection
-mongoose.connect("mongodb://10.0.100.242:27017/", )
+// mongodb://10.0.100.242:27017/yoooo
+mongoose.connect("mongodb://localhost:27017/ScanMe", )
 .then(() => {
   console.log('✅ Connected to MongoDBkkkkkmmmm');
-
-
   app.listen(PORT, () => console.log(`✅ Server running on http://localhost:${PORT}`));
 })
 .catch(err => console.error('❌ MongoDB connection error:', err));
