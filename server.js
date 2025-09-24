9418cb3488b4699504f8a97c88920ba6858328d7
mohamedamine-code <mohamedaminebensir09@gmail.com>
2025-09-24 22:43:57 +0100

    push

--- server.js
+++ server.js
@@ -25,6 +25,8 @@ app.use('/components', componentRoutes);
 // MongoDB connection
 // mongodb://10.0.100.242:27017/yoooos
 mongoose.connect(process.env.MONGO_URI)
+
+
 .then(() => {
   console.log('✅ Connected to MongoDBkkkkkmmmm');
   app.listen(PORT, () => console.log(`✅ Server running on http://localhost:${PORT}`));
